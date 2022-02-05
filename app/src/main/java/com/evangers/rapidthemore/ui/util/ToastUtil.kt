2630719305ae6de3035ecc7e799a32e3834ab545
SunGuk <sgk0713@gmail.com>
2022-02-05 17:50:33 +0900

    remove duplicated toast object

--- app/src/main/java/com/evangers/rapidthemore/ui/util/ToastUtil.kt
+++ app/src/main/java/com/evangers/rapidthemore/ui/util/ToastUtil.kt
@@ -3,6 +3,24 @@ package com.evangers.rapidthemore.ui.util
 import android.content.Context
 import android.widget.Toast
 
+var toast: Toast? = null
+
 fun Context.shortToast(msg: String) {
-    Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+    if (toast == null) toast = Toast(this)
+    toast?.run {
+        cancel()
+        setText(msg)
+        duration = Toast.LENGTH_SHORT
+        show()
+    }
+}
+
+fun Context.longToast(msg: String) {
+    if (toast == null) toast = Toast(this)
+    toast?.run {
+        cancel()
+        setText(msg)
+        duration = Toast.LENGTH_LONG
+        show()
+    }
 }
