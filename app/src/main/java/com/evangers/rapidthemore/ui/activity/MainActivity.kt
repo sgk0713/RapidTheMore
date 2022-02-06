36e587b2a521fc62898c881642ea128a12423b8b
SunGuk <sgk0713@gmail.com>
2022-02-06 12:33:06 +0900

    init admob

--- app/src/main/java/com/evangers/rapidthemore/ui/activity/MainActivity.kt
+++ app/src/main/java/com/evangers/rapidthemore/ui/activity/MainActivity.kt
@@ -3,6 +3,7 @@ package com.evangers.rapidthemore.ui.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.evangers.rapidthemore.R
+import com.google.android.gms.ads.MobileAds
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -11,5 +12,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        MobileAds.initialize(this)
     }
 }
